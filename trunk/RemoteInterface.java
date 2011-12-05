20bdb3c00a1b1f10e208bb024b5617c4530830f7
pranaykothari29@gmail.com <pranaykothari29@gmail.com@9049c153-d108-fda4-d97e-60381aa12f8e>
2011-12-05 15:10:20 +0000

    adding comments

--- trunk/RemoteInterface.java
+++ trunk/RemoteInterface.java
@@ -3,16 +3,21 @@ import java.rmi.RemoteException;
 
 public interface RemoteInterface  extends java.rmi.Remote {
 
+	// returns integer value between 0 through 3 that will be the playerID for caller client. Subsequently, he will be attached to corresponding side of table
 	public int getPlayerID() throws RemoteException;
 	
+	// updates the position of slider for player 'playerID' on server. So that other players could read this position
 	public void updateSlider(int x, int y, int playerID) throws RemoteException;
 	
+	// returns slider coordinates for all the players in game
 	public Slider[] getSliderPositions() throws RemoteException;
 	
 	// returns array with x, y coordinates of Ball
 	public int[] getBallPosition() throws RemoteException;
 
+	// returns 'true' if server has registered four players. Client threads poll and wait this function to be true
 	public boolean hasFourPlayers() throws RemoteException;
 
+	// returns 'true' if player with ID 'playerID' has reached its MAX_GOAL_Limit described in GameConstants file
 	public int terminatePlayer(int playerID) throws RemoteException;
 }
